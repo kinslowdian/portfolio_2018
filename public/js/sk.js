4038b00a65a45acb24b0a072663f252d15558f6f
Simon Kinslow <simonkinslow@gmail.com>
2018-08-30 17:44:34 +0100

    Scroll fix when using js

--- public/js/sk.js
+++ public/js/sk.js
@@ -1,8 +1,29 @@
 // DEBUG
 var trace = function(msg){ console.log(msg); };
 
+var displayList;
+
 function pageLoad_init()
 {
-	trace("pageLoad_init();");
+	// trace("pageLoad_init();");
+
+	nav_init();
 }
 
+function nav_init()
+{
+	displayList = {};
+
+	displayList.header = document.querySelector("header");
+	displayList.footer = document.querySelector("footer");
+}
+
+function request_backToTop(event)
+{
+	displayList.header.scrollIntoView({behavior: 'smooth'});
+}
+
+function request_contact(event)
+{
+	displayList.footer.scrollIntoView({behavior: 'smooth'});
+}
