4e8878356dc79cc90a1e72bf18c02c6768ea6646
Simon Kinslow <simonkinslow@gmail.com>
2018-09-27 20:51:55 +0100

    Hamburger transition into close button added in JS and CSS

--- public/js/sk.js
+++ public/js/sk.js
@@ -2,6 +2,7 @@
 var trace = function(msg){ console.log(msg); };
 
 var displayList;
+var nav_unit;
 
 function pageLoad_init()
 {
@@ -20,8 +21,18 @@ function nav_init()
 {
 	displayList = {};
 
+	nav_unit = {};
+	nav_unit.inUse = false;
+
 	displayList.header = document.querySelector("header");
 	displayList.footer = document.querySelector("footer");
+
+	GFXburger = document.querySelector(".nav-gfx-burger");
+	GFXclose = document.querySelector(".nav-gfx-close");
+
+	displayList.burger = document.querySelector(".nav-burger");
+
+	displayList.burger.addEventListener("click", nav_event, false);
 }
 
 function request_backToTop(event)
@@ -32,4 +43,30 @@ function request_backToTop(event)
 function request_contact(event)
 {
 	displayList.footer.scrollIntoView({behavior: 'smooth'});
+}
+
+function nav_event(event)
+{
+	if(nav_unit.inUse)
+	{
+		nav_unit.inUse = false;
+
+		GFXburger.classList.add("nav-gfx-delay");
+		GFXclose.classList.remove("nav-gfx-delay");
+
+		GFXburger.classList.remove("nav-gfx-hide");
+		GFXclose.classList.add("nav-gfx-hide");
+	}
+
+	else
+	{
+		nav_unit.inUse = true;
+
+		GFXclose.classList.add("nav-gfx-delay");
+		GFXburger.classList.remove("nav-gfx-delay");
+
+
+		GFXclose.classList.remove("nav-gfx-hide");
+		GFXburger.classList.add("nav-gfx-hide");
+	}
 }
